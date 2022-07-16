4ff5750de049cbae43d3f1925bc74204764f5d8b
Jan Sopata <jansopata10@gmail.com>
2022-07-16 16:41:59 +0200

    Implement two Docker containers

--- src/main/java/pl/sopata/springbootdocker/Hello.java
+++ src/main/java/pl/sopata/springbootdocker/Hello.java
@@ -1,13 +1,17 @@
 package pl.sopata.springbootdocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
+    @Value("${envname}")
+    private String envname;
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello!";
+        return "Hello! " + envname;
     }
 }
